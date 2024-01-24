fd89ce2fd82dacc8217f2a0d5cf8f6d3335b5a2d
Jeevan501 <jeevana591501@gmail.com>
2024-01-25 01:03:00 +0530

    Adding views

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 4321;
+const ejs = require("ejs");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-  res.send("Hello im from SPACE");
-});
-
-app.get("/data", (req, res) => {
-  res.send("Hello im from Mars");
+  res.render("home"); // Assuming "home" is the correct name of your EJS file without the "view/" prefix
 });
 
 app.listen(port, () => {
-  console.log("server running", port);
+  console.log("Server running on port", port);
 });
