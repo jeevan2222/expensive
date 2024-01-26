06c991c460d0ff79f7018b94176e06a854f81271
Jeevan501 <jeevana591501@gmail.com>
2024-01-27 00:32:32 +0530

    Enabling Cors

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ const app = express();
 const port = 4321;
 const ejs = require("ejs");
 const path = require("path");
+const cors = require("cors");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 const sequelize = require("../src/dbconnection/db");
 const { verifyEmail, createUser } = require("./Model/user.model");
 
