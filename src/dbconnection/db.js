0c909a9aaa36ca5568ed43cd8a4df706439e01cc
jeevanK <jeevankumark@zen3.com>
2024-10-26 20:32:13 +0530

    Added remote postgres urls

--- src/dbconnection/db.js
+++ src/dbconnection/db.js
@@ -1,7 +1,22 @@
 const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize('Test', 'postgres', 'admin', {
-  host: 'localhost',
-  dialect:'postgres'
+
+const sequelize = new Sequelize('mymoneytracker', 'mymoneytracker_user', 'otIbgQngZh9vEfJvT6Ne0K1JYyN2lnCx', {
+  host: 'dpg-cseg30rtq21c738b6m3g-a.oregon-postgres.render.com',
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
 
 module.exports = sequelize;
